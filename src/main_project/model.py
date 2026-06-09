cb83dad0cb462e27f15d6e450ab1e8c7444e06f7
WilliamKentoRasmussen <s245310@dtu.dk>
2026-06-09 11:54:07 +0200

    updated evaluate

--- src/main_project/model.py
+++ src/main_project/model.py
@@ -48,6 +48,7 @@ class AEv2(eqx.Module):
         out = self.decoder(z)
         return out, z
 
+
 BATCH_SIZE = 64
 LEARNING_RATE = 3e-4
 STEPS = 300
@@ -58,7 +59,6 @@ class targetClassifier(eqx.Module):
     CNN: eqx.Module
 
     def __init__(self, key):
-
         super().__init__()
 
         key_split = jax.random.split(key, 4)
@@ -67,21 +67,22 @@ class targetClassifier(eqx.Module):
             (
                 eqx.nn.Conv2d(1, 3, kernel_size=4, key=key_split[0]),
                 eqx.nn.MaxPool2d(kernel_size=2),
-                eqx.nn.Lambda(jax.nn.relu),      
-                eqx.nn.Lambda(jnp.ravel),        
+                eqx.nn.Lambda(jax.nn.relu),
+                eqx.nn.Lambda(jnp.ravel),
                 eqx.nn.Linear(1728, 512, key=key_split[1]),
-                eqx.nn.Lambda(jax.nn.sigmoid),   
+                eqx.nn.Lambda(jax.nn.sigmoid),
                 eqx.nn.Linear(512, 64, key=key_split[2]),
-                eqx.nn.Lambda(jax.nn.relu),      
+                eqx.nn.Lambda(jax.nn.relu),
                 eqx.nn.Linear(64, 10, key=key_split[3]),
-                eqx.nn.Lambda(jax.nn.log_softmax),  
+                eqx.nn.Lambda(jax.nn.log_softmax),
             )
         )
 
-    def __call__(self,x):
-        #Train function flattens images, hence we have to restore the shape for the CNN.
+    def __call__(self, x):
+        # Train function flattens images, hence we have to restore the shape for the CNN.
         x = x.reshape(1, 28, 28)
         return self.CNN(x)
+
 
 if __name__ == "__main__":
     seed = 3456
