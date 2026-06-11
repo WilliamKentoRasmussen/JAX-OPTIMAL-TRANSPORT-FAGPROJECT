3813bae3b16ed0d3df57cb8be67d5f337939e0e9
Kerem Ozemre <s244794@dtu.dk>
2026-06-11 08:58:27 +0200

    smallchange

--- src/main_project/trainnew.py
+++ src/main_project/trainnew.py
@@ -137,7 +137,7 @@ class AETrainer:
         if re.match(r"ae_best_model_bo_\d+", model_name):
             save(model=model, name=model_name)
             df = pd.DataFrame(history)
-            df.to_csv("training_history.csv", index=False)
+            df.to_csv(f"training_history_{model_name}.csv", index=False)
 
         return model, history, test_loss
 
